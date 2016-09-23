56b02d74fafad587248b306363d928e0df3d839b
Xin Sun <accessun@hotmail.com>
2016-09-23 18:00:05 +0800

    added some utility methods

--- src/io/github/accessun/util/DSAUtils.java
+++ src/io/github/accessun/util/DSAUtils.java
@@ -1,6 +1,7 @@
 package io.github.accessun.util;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * This class provides some of the static utility methods needed by other
@@ -14,7 +15,57 @@ public class DSAUtils {
     private DSAUtils() {
     }
 
-    public static <T> void printArray(T[] arr) {
+    /**
+     * Print out an array of integers onto stdout. This method prints the array
+     * in one line. And each integer in the array is separated by a comma.
+     * 
+     * @param arr
+     */
+    public static void printArray(int[] arr) {
         System.out.println(Arrays.toString(arr));
+    }
+
+    /**
+     * Print out an array of integers within a specified range onto stdout. This
+     * method prints the array in one line. And each integer in the array is
+     * separated by a comma.
+     * 
+     * @param arr
+     * @param from
+     *            the index where the printing started (inclusive)
+     * @param to
+     *            the index where the printing ended (exclusive)
+     */
+    public static void printArray(int[] arr, int from, int to) {
+        int[] tmp = Arrays.copyOfRange(arr, from, to);
+        System.out.println(Arrays.toString(tmp));
+    }
+
+    /**
+     * Exchange the integers at two different position in an array
+     * 
+     * @param arr
+     *            the array to be operated upon
+     * @param i
+     *            the index of first integer to be exchanged
+     * @param j
+     *            the index of second integer to be exchanged
+     */
+    public static void exch(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * Randomly permute an array of integers
+     * 
+     * @param arr
+     */
+    public static void shuffle(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int randInt = new Random().nextInt(arr.length - i) + i;
+            exch(arr, i, randInt);
+        }
     }
 }
